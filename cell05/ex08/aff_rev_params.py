490da5ed4c47f685e47c59e1600ee13b31c6aa61
Nirapada-p <knirphada2549@gmail.com>
2024-11-20 03:10:02 +0000

    pt

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,8 @@
+import sys
+
+x = sys.argv
+if len(x) > 1:
+    for arg in x[1:]:
+        print(arg.upper())
+else:
+    print("none")
---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,11 @@
+import sys 
+if len(sys.argv) != 3:
+    print("none")
+else:
+    key = sys.argv[1]
+    search = sys.argv[2]
+    count_search = search.count(key)
+    if count_search == 0:
+        print("none")
+    else:
+        print(count_search)
