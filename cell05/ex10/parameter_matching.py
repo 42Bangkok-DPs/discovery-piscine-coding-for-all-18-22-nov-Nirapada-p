f8d6560fcb6e8f55f77b532c882d56e8f37590d0
Nirapada-p <knirphada2549@gmail.com>
2024-11-20 06:18:52 +0000

    parame

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    parameter = sys.argv[1]
+    user = input("What was the parameter? ")
+    if user == parameter:
+        print("Good job!")
+    else:
+            print("Nope, sorry...")
