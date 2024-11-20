485220576bd10966744b98c2ef5e5bc5f5b7094c
Nirapada-p <knirphada2549@gmail.com>
2024-11-20 15:28:36 +0000

    none

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,7 @@
+import sys
+if len(sys.argv) != 1:
+    print("none")
+else:
+    for i in range(0,11):
+        show = (f"Table de {i} : {0*1} {1*i} {2*i} {3*i} {4*i} {5*i} {6*i} {7*i} {8*i} {9*i} {10*i}")
+        print(show)
